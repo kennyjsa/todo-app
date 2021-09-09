d83554ce0e29a63cf0b9abdf4a4fff51f592091e
Kenny Johnson <kenny.barao@gmail.com>
2021-09-09 07:26:02 -0400

    end style todo filter

--- src/components/todo/TodoFilter/index.tsx
+++ src/components/todo/TodoFilter/index.tsx
@@ -4,10 +4,13 @@ import styles from './styles.module.css'
 
 export const TodoFilter: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.link}>All</div>
-      <div className={styles.link}>Active</div>
-      <div className={styles.link}>Completed</div>
-    </div>
+    <>
+      <div className={styles.container}>
+        <div className={`${styles.link} ${styles.active}`}>All</div>
+        <div className={styles.link}>Active</div>
+        <div className={styles.link}>Completed</div>
+      </div>
+      <div className={styles.text}>Drag and drop to reorder list</div>
+    </>
   )
 }
