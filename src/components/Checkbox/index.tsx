e705988415ebbe6e476c0cd583ba687f38c510ce
Kenny Johnson <kenny.barao@gmail.com>
2021-09-11 10:44:09 -0400

    checkbox add onclick event and adjustment in css

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -5,11 +5,17 @@ import styles from './styles.module.css'
 
 interface CheckboxProps {
   checked: boolean
+  onClick: () => void
 }
 
-export const Checkbox: React.FC<CheckboxProps> = ({ checked }) => {
+export const Checkbox: React.FC<CheckboxProps> = ({ checked, onClick }) => {
   return (
-    <div className={`${styles.checkbox} ${checked ? styles.checked : ''}`}>
+    <div
+      className={`${styles.checkbox} ${checked ? styles.checked : ''}`}
+      role="checkbox"
+      //aria-checked={styles.checked}
+      onClick={onClick}
+    >
       {checked && (
         <Image src="/assets/images/icon-check.svg" width="9px" height="8px" />
       )}
