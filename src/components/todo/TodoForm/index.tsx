e9f558ada2cefd7201a1cf8fecd4af43ad1a0cf5
Kenny Johnson <kenny.barao@gmail.com>
2021-09-11 10:15:52 -0400

    todo form add on idb

--- src/components/todo/TodoForm/index.tsx
+++ src/components/todo/TodoForm/index.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
+import { useTodos } from '../../../hooks/useTodos'
 
 import styles from './styles.module.css'
 
 export const TodoForm: React.FC = () => {
+  const { addTodo } = useTodos()
+  const [text, setText] = useState('')
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault()
+
+    if (text.trim() === '') {
+      alert('You need to enter the task')
+      return
+    }
+
+    addTodo(text)
+    setText('')
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className={styles.todoForm}>
         <div className={styles.icon}></div>
-        <input type="text" placeholder="Create a new todo..." />
+        <input
+          type="text"
+          placeholder="Create a new todo..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
     </form>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
 import type { NextPage } from 'next'
 
+import { TodoContextProvider } from '../contexts/TodoContext'
+
 import { TodoForm } from '../components/todo/TodoForm'
 import { TodoList } from '../components/todo/TodoList'
 import { TodoFilter } from '../components/todo/TodoFilter'
 
 const Home: NextPage = () => {
   return (
-    <>
+    <TodoContextProvider>
       <TodoForm />
       <TodoList />
       <TodoFilter />
-    </>
+    </TodoContextProvider>
   )
 }
 
