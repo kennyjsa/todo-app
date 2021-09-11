380bde080856b4048b4efc80b8dd4560a11d1168
Kenny Johnson <kenny.barao@gmail.com>
2021-09-11 10:16:25 -0400

    todo items list from idb

--- src/components/todo/TodoList/index.tsx
+++ src/components/todo/TodoList/index.tsx
@@ -1,43 +1,46 @@
 import React from 'react'
+import { useTodos } from '../../../hooks/useTodos'
 import { TodoControlBar } from '../TodoControlBar'
 import TodoItem from '../TodoItem'
 
 import styles from './styles.module.css'
 
 export const TodoList: React.FC = () => {
-  const todos = [
-    {
-      completed: true,
-      text: 'Complete online JavaScript course'
-    },
-    {
-      completed: false,
-      text: 'Jog around the park 3x'
-    },
-    {
-      completed: false,
-      text: '10 minutes meditation'
-    },
-    {
-      completed: false,
-      text: 'Read for 1 hour'
-    },
-    {
-      completed: false,
-      text: 'Pick up groceries'
-    },
-    {
-      completed: false,
-      text: 'Complete Todo App on Frontend Mentor'
-    }
-  ]
+  const { todos } = useTodos()
 
   return (
     <div className={styles.todoList}>
       {todos.map((x) => (
-        <TodoItem key={x.text} completed={x.completed} text={x.text} />
+        <TodoItem key={x.id} completed={x.completed} text={x.text} />
       ))}
       <TodoControlBar />
     </div>
   )
 }
+
+const __todos = [
+  {
+    completed: true,
+    text: 'Complete online JavaScript course'
+  },
+  {
+    completed: false,
+    text: 'Jog around the park 3x'
+  },
+  {
+    completed: false,
+    text: '10 minutes meditation'
+  },
+  {
+    completed: false,
+    text: 'Read for 1 hour'
+  },
+  {
+    completed: false,
+    text: 'Pick up groceries'
+  },
+  {
+    completed: false,
+    text: 'Complete Todo App on Frontend Mentor'
+  }
+]
