3df8ad2918b6238ec49aeefbf31c86bd3b67db74
Kenny Johnson <kenny.barao@gmail.com>
2021-09-14 06:24:16 -0400

    gh-pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== 'production'
+
 module.exports = {
-  reactStrictMode: false
+  reactStrictMode: true,
+
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' }
+    }
+  },
+  basePath: !debug ? '/todo-app' : '',
+  assetPrefix: !debug ? '/todo-app/' : '',
+
+  webpack: (config, { dev }) => {
+    // Perform customizations to webpack config
+    // console.log('webpack');
+    // console.log(config.module.rules, dev);
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
+      }
+      return rule
+    })
+    // Important: return the modified config
+    return config
+  }
 }
